97a1a0933bf12ffb733759189e78a57532da8603
ThiagoCastagnazzi <thiagocastagnazzi@gmail.com>
2022-10-18 15:51:06 -0400

    feat: done all to do tasks

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -16,17 +16,27 @@ interface CardsProps {
 }
 
 export function CardList({ cards }: CardsProps): JSX.Element {
-  // TODO MODAL USEDISCLOSURE
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [imgUrl, setImgUrl] = useState('');
 
-  // TODO SELECTED IMAGE URL STATE
-
-  // TODO FUNCTION HANDLE VIEW IMAGE
+  function handleViewImage(url: string): void {
+    setImgUrl(url);
+    onOpen();
+  }
 
   return (
     <>
-      {/* TODO CARD GRID */}
+      <SimpleGrid columns={3} spacing="40px">
+        {cards?.map(card => (
+          <Card
+            data={card}
+            viewImage={() => handleViewImage(card.url)}
+            key={card.id}
+          />
+        ))}
+      </SimpleGrid>
 
-      {/* TODO MODALVIEWIMAGE */}
+      <ModalViewImage imgUrl={imgUrl} isOpen={isOpen} onClose={onClose} />
     </>
   );
 }
